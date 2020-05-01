7836c39376cfe31caf82d5fafc7c33ee31dc8dfa
Lucas Viana <lucasviana112@gmail.com>
2020-05-01 17:46:34 -0300

    calculo desconto e formatação objeto de resposta da aplicação do desafio do carrinho de compras

--- nodejs-12/src/index.js
+++ nodejs-12/src/index.js
@@ -1,54 +1,40 @@
 const promotions = ["SINGLE LOOK", "DOUBLE LOOK", "TRIPLE LOOK", "FULL LOOK"];
 const data = require("./data/products.json");
-const { products } = data;
+// const { products } = data;
 
 function getShoppingCart(ids, productsList) {
   let productsFound = [];
+  let products = [];
   productsList.forEach((item) => {
     if (ids.indexOf(item.id) !== -1) {
       productsFound.push(item);
     }
   });
 
-  const promotionFound = verifyPromotions(productsFound);
-  //   console.log(promotionFound);
-  //   console.log(productsFound);
-  // function ini
-  let totalPrice = 0;
-  let discountValue = 0;
-  let discount = "0%";
-  if (promotionFound === "SINGLE LOOK") {
-    for (let i in productsFound) {
-      let normalPrice = productsFound[i].regularPrice;
-      let promotionPrice = false;
-      //   console.log(productsFound[i]);
-      productsFound[i].promotions.forEach((item) => {
-        if (item.looks.indexOf("SINGLE LOOK") !== -1) {
-          promotionPrice = true;
-          totalPrice += item.price;
-          // descontos
-          discountValue += normalPrice - item.price;
-          // CALCULAR DESCONTO EM PORCENTAGEM
-        }
-      });
-      if (!promotionPrice) {
-        totalPrice += normalPrice;
-      }
-    }
-  }
-  console.log("totalPrice");
-  console.log(totalPrice);
-  console.log("discountValue");
-  console.log(discountValue);
-  // function fim
+  const promotion = verifyPromotions(productsFound, promotions);
+  const priceAndDiscount = getPriceAndDiscount(promotion, productsFound);
+  const { totalPrice, discountValue, discount } = priceAndDiscount;
 
-  return {};
+  productsFound.forEach((item) => {
+    products.push({
+      name: item.name,
+      category: item.category,
+    });
+  });
+
+  return {
+    products,
+    promotion,
+    totalPrice,
+    discountValue,
+    discount,
+  };
 }
 
-const verifyPromotions = (productsFound) => {
+const verifyPromotions = (productsFound, promotions) => {
   // T-SHIRTS, PANTS, SHOES, BAGS
   let allCategories = [];
-  let promotion = "";
+  let promotionSearch = "";
 
   for (let i = 0; i < productsFound.length; i++) {
     switch (productsFound[i].category) {
@@ -72,20 +58,48 @@ const verifyPromotions = (productsFound) => {
   });
 
   if (categories.length === 4) {
-    promotion = "FULL LOOK";
+    promotionSearch = promotions[3];
   } else if (categories.length === 3) {
-    promotion = "TRIPLE LOOK";
+    promotionSearch = promotions[2];
   } else if (categories.length === 2) {
-    promotion = "DOUBLE LOOK";
+    promotionSearch = promotions[1];
   } else if (categories.length === 1) {
-    promotion = "SINGLE LOOK";
+    promotionSearch = promotions[0];
   }
 
-  //   console.log(allCategories);
-  //   console.log(categories);
-  return promotion;
+  return promotionSearch;
 };
 
-getShoppingCart([110, 120, 130, 140], products);
+const getPriceAndDiscount = (promotionFound, productsFound) => {
+  let totalPrice = 0;
+  let discountValue = 0;
+  let discount = null;
+  for (let i in productsFound) {
+    let normalPrice = productsFound[i].regularPrice;
+    let promotionPrice = false;
+    productsFound[i].promotions.forEach((item) => {
+      if (item.looks.indexOf(promotionFound) !== -1) {
+        promotionPrice = true;
+        totalPrice += item.price;
+        discountValue += normalPrice - item.price;
+      }
+    });
+    if (!promotionPrice) {
+      totalPrice += normalPrice;
+    }
+  }
+  discount = (discountValue * 100) / (totalPrice + discountValue);
+  discount = discount.toFixed(2) + "%";
+  discountValue = discountValue.toFixed(2);
+  totalPrice = totalPrice.toFixed(2);
+
+  return {
+    totalPrice,
+    discountValue,
+    discount,
+  };
+};
+
+// getShoppingCart([110, 130, 140, 230, 310, 330], products);
 
 module.exports = { getShoppingCart };
