2e344a72105c1bfe68eeefe258adaa1ba8e4d659
Lucas Viana <lucasviana112@gmail.com>
2020-05-24 11:21:07 -0300

    remoção layout que sobrou sem componentização

--- react-13/src/components/Contacts.jsx
+++ react-13/src/components/Contacts.jsx
@@ -9,22 +9,7 @@ class Contacts extends React.Component {
 
   renderContacts() {
     let listContacts = this.props.contacts.map((contact) => {
-      return (
-        // <article className="contact">
-        //   <span className="contact__avatar">
-        //     <img src={item.avatar} alt="avatar" />
-        //   </span>
-        //   <span className="contact__data">{item.name}</span>
-        //   <span className="contact__data">{item.phone}</span>
-        //   <span className="contact__data">{item.country}</span>
-        //   <span className="contact__data">
-        //     {this.getDate(item.admissionDate)}
-        //   </span>
-        //   <span className="contact__data">{item.company}</span>
-        //   <span className="contact__data">{item.department}</span>
-        // </article>
-        <Contact key={contact.id} data={contact} />
-      );
+      return <Contact key={contact.id} data={contact} />;
     });
     return listContacts;
   }
