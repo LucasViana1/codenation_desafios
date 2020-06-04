b1a3b6efbc3bc8cf99095dffc93a9fa53ff7878c
Lucas Viana <lucasviana112@gmail.com>
2020-06-03 22:30:44 -0300

    definição rotas macro de dashboar, login e authorize, e rotas internas de playlist e tracks

---
+++ react-17/src/routes/AuthorizeRoute.jsx
@@ -0,0 +1,43 @@
+import React, { useEffect, useState } from "react";
+import { Redirect } from "react-router-dom";
+import { useDispatch, useSelector } from "react-redux";
+
+import { authError, authSucess } from "../actions";
+
+import { getInfoFromUrlHash } from "../modules/url";
+
+import { Authorize } from "../containers";
+
+const AuthorizeRoute = () => {
+  const [redirect, setRedirect] = useState(false);
+  const isLogged = useSelector((state) => state.auth.isLogged);
+  const dispatch = useDispatch();
+
+  const urlHash = window.location.hash;
+
+  useEffect(() => {
+    const hashData = getInfoFromUrlHash(urlHash);
+
+    if (hashData.error) {
+      dispatch(authError(hashData.error));
+
+      return;
+    }
+
+    dispatch(authSucess(hashData));
+  }, [dispatch, urlHash]);
+
+  useEffect(() => {
+    if (isLogged) {
+      setTimeout(() => setRedirect(true), 3000);
+    }
+  }, [isLogged]);
+
+  if (redirect) {
+    return <Redirect to={{ pathname: "/dashboard" }} />;
+  }
+
+  return <Authorize />;
+};
+
+export default AuthorizeRoute;
