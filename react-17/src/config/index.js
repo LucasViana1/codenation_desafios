6a92553683e67f524f00b32e934a47bebc592f56
Lucas Viana <lucasviana112@gmail.com>
2020-06-03 23:03:00 -0300

    variaveis de ambiente

--- react-17/src/config/index.js
+++ react-17/src/config/index.js
@@ -1,13 +1,11 @@
+import { tokenId } from "./env";
+
 export const config = {
   spotify: {
-    authorizationURL: 'https://accounts.spotify.com/authorize',
-    clientId: '',
+    authorizationURL: "https://accounts.spotify.com/authorize",
+    clientId: tokenId,
     redirectUrl: `${window.location.origin}/authorize`,
-    webAPI: 'https://api.spotify.com/v1',
-    scopes: [
-      'user-read-email',
-      'user-read-private',
-      'streaming'
-    ]
-  }
-}
+    webAPI: "https://api.spotify.com/v1",
+    scopes: ["user-read-email", "user-read-private", "streaming"],
+  },
+};
