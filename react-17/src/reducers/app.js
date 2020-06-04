e31ee62dd5a9c146c176beeb06e71b44e49154f8
Lucas Viana <lucasviana112@gmail.com>
2020-06-03 23:07:30 -0300

    definição reducers

---
+++ react-17/src/reducers/app.js
@@ -0,0 +1,11 @@
+import packageJson from "../../package.json";
+
+const INITIAL_STATE = {
+  environment: process.env.NODE_ENV,
+  name: packageJson.name,
+  version: packageJson.version,
+};
+
+const appReducer = (state = INITIAL_STATE) => state;
+
+export default appReducer;
